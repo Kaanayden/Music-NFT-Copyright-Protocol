df6972b6f04571ae825f73913823bb9635f90180
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2022-08-05 18:52:46 +0300

    navbar updated

---
+++ website/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import WalletConnection from "../components/WalletConnection/WalletConnection";
+
+const Home = ()=>{
+    return(
+    <>
+    </>
+    )
+}
+
+export default Home;
