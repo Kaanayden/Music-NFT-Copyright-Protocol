d0c60ef4204ac3077f5ff500aabd4d07f92b7359
koybasimuhittin <100305964+koybasimuhittin@users.noreply.github.com>
2022-08-06 10:13:21 +0300

    homepage updated

--- template/ethereum-boilerplate-main/src/components/Pages/Home/Home.jsx
+++ template/ethereum-boilerplate-main/src/components/Pages/Home/Home.jsx
@@ -2,6 +2,14 @@ import { useMoralis } from "react-moralis";
 import { Layout } from "antd";
 import { Header } from "antd/lib/layout/layout";
 import Copyright from "../../images/copyright.jpg";
+import Whatsapp from "../../images/whatsapp.png";
+import Lock from "../../images/lock.jpg";
+import zora from "../../images/zora.png";
+import lit from "../../images/lit.png";
+import nft from "../../images/nft.png";
+import polygon from "../../images/polygon.png";
+import kaan from "../../images/kaan.png";
+import muhi from "../../images/muhi.png";
 import "./Home.css";
 
 export default function Home() {
@@ -9,16 +17,43 @@ export default function Home() {
 
   return (
     <div className="container">
-      <div className="outer">
+      <div className="header">
         <img src={Copyright}/>
         <div className="cover">
          <p className="description">Music NFT Copyright Protocol</p>
          <button className="moreinfo" onClick={()=>{window.open("https://github.com/Kaanayden/Music-NFT-Copyright-Protocol","_blank")}}>More Info</button>
         </div>
-        <div className="inner">
-          <div className="logo"></div>
-        </div>
       </div>
+      <div className="inner">
+          <br/>
+          <div className="sponsor">
+            <div className="logo"></div>
+            <p>Sponsors</p>
+            <div className="inner">
+              <img src={zora}/>
+              <img src={polygon}/>
+              <img src={lit}/>
+              <img src={nft}/>
+            </div>
+          </div>
+          <p>More Description</p>
+            <div className="animation">
+              <img className="wp" src={Whatsapp}/>
+              <img className="lock" src={Lock}/>
+            </div>
+          <br/>
+          <p>More Description</p>
+        </div>
+        <div className="contact">
+          <div className="kaan">
+            <img src={kaan}/>
+            <p>Kaan Aydeniz <br/> Linkedin
+            </p>
+          </div>
+          <div className="muhittin">
+            <img src={muhi}/>
+          </div>
+        </div>
     </div>
   );
 }
